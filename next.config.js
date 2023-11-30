abc2dd53fcf16c9fa826a7980a40179047bd10cb
odinellefsen <49004766+odinellefsen@users.noreply.github.com>
2023-11-30 15:01:25 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
     output: 'standalone',
+    reactStrictMode: true,
     basePath: isProd ? '/csv_to_webhook' : '',
     assetPrefix: isProd ? '/csv_to_webhook/' : '',
 };
