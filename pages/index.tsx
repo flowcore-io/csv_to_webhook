bfafdd860c580c0834158fe7339ad6bd92f3e857
odinellefsen <49004766+odinellefsen@users.noreply.github.com>
2023-12-01 11:40:56 +0000

    added title and description

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ type Line = string;
 type Header = string;
 type JSONLine = { [key: string]: string };
 let sent: number = 0
-const batchLineCount: number = 125
+const batchLineCount: number = 100
 const concurrency: number = 50
 
 export default function Home() {
@@ -127,38 +127,40 @@ export default function Home() {
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-    <form className="w-full max-w-lg bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-      <div className="mb-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="file">
-          CSV File
-        </label>
-        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="file" onChange={handleFileSelection} />
-      </div>
-      <div className="mb-6">
-        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="webhook">
-          Webhook URL
-        </label>
-        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="webhook" type="text" value={webhookUrl} onChange={handleWebhookUrlChange} />
-      </div>
-      <div className="flex items-center justify-between">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={handleFileUpload}>
-          Start
-        </button>
-        <div className="w-full max-w-lg flex justify-end items-center mt-4">
-          <Link href="https://www.flowcore.com" target="_blank" rel="noopener noreferrer" className="text-sm text-gray-700 mr-2">
-            Powered by Flowcore
-          </Link>
-          <Image src="/favicon.ico" alt="Flowcore icon" width={16} height={16} />
+      <h1 className="w-full max-w-lg text-gray-700 flex justify-center text-2xl">Stream a CSV file to a webhook</h1>
+      <p className="w-full max-w-lg text-gray-700 flex justify-center text-xs mb-4">The program streams a CSV file, converting its contents into JSON objects with batches of {batchLineCount} records each, and then POSTs these batches, using concurrency to a specified webhook URLutilizing a concurrency level of {concurrency}.</p>
+      <form className="w-full max-w-lg bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="file">
+            CSV File
+          </label>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="file" onChange={handleFileSelection} />
         </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="webhook">
+            Webhook URL
+          </label>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="webhook" type="text" value={webhookUrl} onChange={handleWebhookUrlChange} />
+        </div>
+        <div className="flex items-center justify-between">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={handleFileUpload}>
+            Start
+          </button>
+          <div className="w-full max-w-lg flex justify-end items-center mt-4">
+            <Link href="https://www.flowcore.com" target="_blank" rel="noopener noreferrer" className="text-sm text-gray-700 mr-2">
+              Powered by Flowcore
+            </Link>
+            <Image src="/favicon.ico" alt="Flowcore icon" width={16} height={16} />
+          </div>
+        </div>
+      </form>
+      <div className="w-full max-w-lg">
+        <p className="text-gray-700 mb-2">Status: {status}</p>
+      <div className="bg-gray-200 rounded h-4 overflow-hidden">
+        <div className="bg-blue-600 h-4" style={{ width: `${processedLines.toFixed(0)}%` }} />
       </div>
-    </form>
-    <div className="w-full max-w-lg">
-      <p className="text-gray-700 mb-2">Status: {status}</p>
-    <div className="bg-gray-200 rounded h-4 overflow-hidden">
-      <div className="bg-blue-600 h-4" style={{ width: `${processedLines.toFixed(0)}%` }} />
-    </div>
-      <p className="text-gray-700 mt-2">{`Progress: ${processedLines.toFixed(0)}/100`}</p> {/* Replace 100 with totalLines if available */}
-    </div>
+        <p className="text-gray-700 mt-2">{`Progress: ${processedLines.toFixed(0)}/100`}</p> {/* Replace 100 with totalLines if available */}
+      </div>
     </main>
   );
 }
